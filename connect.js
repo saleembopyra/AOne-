8fd9a16a4143dfe034cd6379543ab7459f1d8bb3
saleembopyra <154544735+saleembopyra@users.noreply.github.com>
2024-09-01 08:00:07 +0500

    Update connect.js

--- connect.js
+++ connect.js
@@ -4,13 +4,13 @@
 ░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
 
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @project_name AOne-MD
+* @authors AOne & sachibot-team(Sachintha) <https://github.com/prabathLK>
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
-* @link <https://github.com/prabathLK/PRABATH-MD>
+* @link <https://github.com/saleembopyra/AOne>
 * @version 2.1.2
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2023 AOne, SACHI-BOT. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
